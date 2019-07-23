8ba26b625f8c98b4dc0877e5d6279442bda593eb
Tay <taypih@gmail.com>
2019-07-23 03:43:55 -0400

    Update MainActivity to show a list of images

--- app/src/main/java/com/udacity/sandwichclub/MainActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/MainActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import com.udacity.sandwichclub.adapter.CustomAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,9 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] sandwiches = getResources().getStringArray(R.array.sandwich_names);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, sandwiches);
+        String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
+        ListAdapter adapter = new CustomAdapter(this, sandwiches);
 
         // Simplification: Using a ListView instead of a RecyclerView
         ListView listView = findViewById(R.id.sandwiches_listview);
